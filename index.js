8f854fc4fe45bcdd1a3f7bbe0e5670c7099a6fe6
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-02 18:48:28 -0400

    created generateShape.js file; started creating function writeToFile -- need to follow up with 'init()' call in index.js file

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Shape = require('./lib/shapes');
+const {generateShape} = require('./lib/generateShape');
 
-inquirer
-    .prompt([
+// Array of question prompts that run in the terminal using inquirer for the user to respond to and generate logo data
+const questions = [
         {
             type: 'input',
             message: 'LOGO TEXT: Please enter up to (3) characters to be displayed on your logo:',
@@ -27,7 +28,28 @@ inquirer
             message: "SHAPE COLOR: Please enter a shape color keyword (OR a hexadecimal number):",
         },
 
-    ])
-    .then((response) =>
-        console.log('Success!') 
-    );
+    ];
+
+    function writeToFile(fileName, data) {
+        console.log("Writing" + data +  "to" + fileName + "file!")
+        fs.writeFile(fileName, data, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success! Please see the 'dist' folder for the logo.svg file you just created!");
+        });
+    }
+
+    // function init() {
+    //     inquirer.prompt(questions)
+    //         .then((data) => {
+    //             const filename = './dist/logo.svg';
+
+    //             fs.writeToFile(filename, (err) =>
+    //             err ? console.log(err) : console.log('Success! Please see the "dist" folder for the logo.svg file you just created!')
+    //             );
+    //         });
+    // }
+
+    // Function call to initialize SVG logo app
+    // init();
